826b69731f113c44d3c5aee74f2829878c077621
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-22 18:26:01 +0100

    Create show_report_sale_of_a_day.sh

---
+++ ProjetDevOps1/show_report_sale_of_a_day.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#File detail of the day:
+x="ventes.csv"
+
+#print the number of all tickets:
+number=$(awk -F',' 'END{ print $1 }' $x)
+echo Number of tickets: $number
+
+#print the sale total of all the shops:
+total_sales_all_shops=$(awk '{ s+=$(NF==2) } END{ print s }' $x)
+echo Total sale of all shops: $total_sales_all_shops
+
+#print the average of sale of the Brand:
+average_sales_all_shop=$(( total_sales_all_shops / number ))
+echo Average sales of all shops: $average_sales_all_shop
+
+#print the total sale of each shop:
