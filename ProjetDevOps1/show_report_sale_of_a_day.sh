38b3e0f31630d3093bc63fbc3388a7e9e94a5be7
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-23 17:08:34 +0100

    Define argument for the script when it be executed

--- ProjetDevOps1/show_report_sale_of_a_day.sh
+++ ProjetDevOps1/show_report_sale_of_a_day.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #File detail of the day:
-x="ventes.csv"
+# $1 is 'the' argument of this script when we run it on cron job
 
 #print the number of all tickets:
-number=$(awk -F',' 'END{ print $1 }' $x)
+number=$(awk -F',' 'END{ print $1 }' $1)
 echo Number of tickets: $number
 
 #print the sale total of all the shops:
-total_sales_all_shops=$(awk 'NR>1 { s+=$($4) } END{ print s }' $x)
+total_sales_all_shops=$(awk 'NR>1 { s+=$($4) } END{ print s }' $1)
 echo Total sale of all shops: $total_sales_all_shops
 
 #print the average of sale of the Brand:
@@ -15,4 +15,4 @@ average_sales_all_shop=$(( total_sales_all_shops / number ))
 echo Average sales of all shops: $average_sales_all_shop
 
 #print the total sale of each shop:
-awk -F',' '{a[$3]+=$4;}END{for(i in a)print "shop " i", "a[i];}' $x
+awk -F',' '{a[$3]+=$4;}END{for(i in a)print "shop " i", "a[i];}' $1
