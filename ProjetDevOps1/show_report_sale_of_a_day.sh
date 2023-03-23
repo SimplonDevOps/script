a2efe378cf671fd05cd771a9bfc8a1ea5badf935
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-24 00:03:01 +0100

    Update show_report_sale_of_a_day.sh

--- ProjetDevOps1/show_report_sale_of_a_day.sh
+++ ProjetDevOps1/show_report_sale_of_a_day.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #File detail of the day:
 # $1 is 'the' argument of this script when we run it on cron job
+
 x=$1
 
 #check if we have any bussiness yesterday
@@ -10,7 +11,7 @@ then
     exit 1
 fi 
 
-#print DATE of data
+#print date of data
 YES_DAT=$(date --date=' 1 days ago' '+%d/%m/%Y')
 echo  REPORT of bussiness  $YES_DAT
 
@@ -29,22 +30,21 @@ echo $payment_visa payments by visa card
 echo $payment_espece payments by cash
 
 #print the number of shops that could sell something yesterday
-z=$(awk -F',' 'NR>1 {a[$3]+=$4;}END{for(i in a)print "Customer " i": "a[i]" Dollars";}' $x | wc -l)
+z=$(awk -F',' 'NR>1 {a[$3]+=$4;}END{for(i in a)print "Customer " i": "a[i]"$";}' $x | wc -l)
 echo $z shops did get bussiness
 
 #print the sale total of all the shops:
 total_sales_all_shops=$(awk 'NR>1 { s+=$($4) } END{ print s }' $x)
-echo $total_sales_all_shops Dollars: Total sales of all shops
+echo $total_sales_all_shops $: Total sales of all shops
 
 #print average sale of each shop chiffre d'affaire de chaque magasin
 average_sales_each_shop=$(( total_sales_all_shops / z ))
-echo $average_sales_each_shop Dollars: Average sale per shop 
+echo $average_sales_each_shop $: Average sale per shop 
 
 #print the average of how many money that the custom pay each time:
 average_sales_all_shop=$(( total_sales_all_shops / number ))
-echo $average_sales_all_shop Dollars: average of bill  
+echo $average_sales_all_shop $: average of bill  
 
 #print total sale of each shop:
 echo Total sale of each shop:
-awk -F',' 'NR>1 {a[$3]+=$4;}END{for(i in a)print "shopID " i": "a[i]" Dollars";}' $x | column -t 
-
+awk -F',' 'NR>1 {a[$3]+=$4;}END{for(i in a)print "shopID "i": "a[i]"$";}' $x | column -t 
