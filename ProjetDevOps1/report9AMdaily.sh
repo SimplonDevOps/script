662a27107461ace63415ef2c56e81fdda9ebfd20
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-23 20:49:51 +0100

    added less to show report by page

--- ProjetDevOps1/report9AMdaily.sh
+++ ProjetDevOps1/report9AMdaily.sh
@@ -2,4 +2,4 @@
 
 #the agu
 sudo crontab -e
-09*** /home/camchau/ProjetDevOps1/show_report_sales_of_a_day.sh generates.ph
+09*** /home/camchau/ProjetDevOps1/show_report_sales_of_a_day.sh generates.ph | less
