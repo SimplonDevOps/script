9618b2a9b15cddd2187bee983de847ee8459726b
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-23 23:55:30 +0100

    Correct name of python file

--- ProjetDevOps1/report9AMdaily.sh
+++ ProjetDevOps1/report9AMdaily.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#the agu
+#generates.py is the argument of show_report_sale_of_a_day.sh
+
 sudo crontab -e
-09*** /home/camchau/ProjetDevOps1/show_report_sales_of_a_day.sh generates.ph | less
+09*** /home/camchau/ProjetDevOps1/show_report_sales_of_a_day.sh generates.py | less
