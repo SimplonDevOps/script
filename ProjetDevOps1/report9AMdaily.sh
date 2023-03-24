7fa74276c55acad8c60db366a77898c38dcf4594
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-24 11:19:04 +0100

    correct words

--- ProjetDevOps1/report9AMdaily.sh
+++ ProjetDevOps1/report9AMdaily.sh
@@ -3,25 +3,27 @@
 
 sudo crontab -e
 
+#capture the path
 place=$(pwd)
-chmod a+x generates.py
+chmod a+x generates.py show_report_sale_of_a_day.sh
 
-#create a Folder: everyday the generates.py will be run to ceate a new .csv file. This Folder is to group all those .csv file
+#create a Folder and go inside it: everyday the generates.py will be run to create a new .csv file. This Folder is to group all those .csv file
 mkdir ReportData
 cd ./ReportData 
 
-#move the show_report_sales_of_a_day.sh to the Folder ReportData 
-mv $place/show_report_sales_of_a_day.sh $place/ReportData
+#move the show_report_sale_of_a_day.sh to the Folder ReportData 
+mv $place/show_report_sale_of_a_day.sh $place/ReportData
 
-#create a .csv file automatically in the Report Folder at 9AM daily 
+#We are inside ReportData Folder, now create a .csv file automatically at 9AM daily 
 09*** $place/generates.py 
 
 
-#Find the .csv file has just created at 9AM 
+#Find the .csv file has just created at 9AM and put it in a variable
 YES_DAT=$(date --date=' 1 days ago' '+%m_%d_%Y')
 yes_report=$(ls | grep $YES_DAT)
 
-#Run the show_report_sales_of_a_day.sh with the .csv file has just created at 9AM 
-59***./$place/ReportData/show_report_sales_of_a_day.sh $yes_report
+#at 09:05, run automatically the show_report_sale_of_a_day.sh with the .csv file has just created at 9AM 
+#Use the variable $yes_report as an argument to the bash file:
+59***./$place/ReportData/show_report_sale_of_a_day.sh $yes_report
 
 
