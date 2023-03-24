31d9884ca61382fb80dcc7d8110f4a56dbe49975
Zaycoo <clement.labuda@gmail.comgit config --global user.email clement.labuda@gmail.comgit config --global user.email clement.labuda@gmail.com>
2023-03-24 09:39:51 -0400

    VIRScript.sh

--- VIRScript.sh
+++ VIRScript.sh
@@ -1,16 +1,19 @@
 #!bin/bash
 
-#APP_PATH=$(dirname $inspath$0))"
-
+#APP_PATH=$(dirname realpath)
+CSV_Files=/home/03_23_2023__08_17_ventes.csv
 #Python3 generate.py
 
-line=03_23_2023__08_17_ventes.csv
+line=CSV_FILES
 
+
+#Récupération de la quatriéme collone (prix des tickets)
 MOLIS=$(awk -F ',' '{print $4}' "$line")
+#Récupération de la Premiere collone du document (Nombre de Tickets)
 NUMTIK=$(awk -F ',' '{print $1}' "$line" | awk 'END{print}')
 
 ###################################################################
-
+#Fonction qui calcule touts les prix des tickets et les ranges dans la variable MOLIS
 function recette () {
 
         for i in "${MOLIS[@]}"
@@ -22,7 +25,7 @@ return $MOLIS
 }
 
 #################################################################
-
+#Fonction qui calcul le nombre de ticket et la recette et le résultat est stocker dans la variable TIKMOY
 function panier_moyen () {
 
         TIKMOY=$(($ALMON/$NUMTIK))
