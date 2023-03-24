52c93c6feab93a10260efdd70a4591c2231c273b
ngocamchau <128331755+ngocamchau@users.noreply.github.com>
2023-03-24 10:27:52 +0100

    change a fix path to a portable path

--- ProjetDevOps1/report9AMdaily.sh
+++ ProjetDevOps1/report9AMdaily.sh
@@ -3,4 +3,12 @@
 #generates.py is the argument of show_report_sale_of_a_day.sh
 
 sudo crontab -e
-09*** /home/camchau/ProjetDevOps1/show_report_sales_of_a_day.sh generates.py | less
+
+place=$(pwd)
+chmod a+x generates.py
+
+#create a Folder: everyday the generates.py will be run to ceate a new .csv file. This Folder is to group all those .csv file
+mkdir ReportData
+cd ./ReportData 
+09*** $place/generates.py 
+
